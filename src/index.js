fafc0fad20276b5d59a0ec65aa6317996c366bf1
xsiadron <xsiadronmc@gmail.com>
2023-05-25 23:56:07 +0200

    Preview getAttendances() functionality

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// -- Example Usage
+
+// const Librus = require("../modules/librusApi.js");
+// const librus = new Librus();
+// librus.authorize('login', 'password')
+//     .then(() => {
+//         return librus.getAttendances();
+//     })
+//     .catch(console.error);
+
