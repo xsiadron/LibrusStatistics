d43181601a14ef312929ab1036d7c83324c6f84c
xsiadron <xsiadronmc@gmail.com>
2023-06-05 16:51:29 +0200

    React.js with Navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './styles/App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="app">
+      {/* 1. Nav */}
+      <Navbar />
+
+      {/* 2. Subnav */}
+
+      {/* 3. List of subjects */}
+
+    </div>
+  );
+}
+
+export default App;
