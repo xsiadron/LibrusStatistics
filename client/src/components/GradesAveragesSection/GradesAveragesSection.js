da4d64948c3bb11b473ecec7f5a6469584ba504c
xsiadron <xsiadronmc@gmail.com>
2023-06-20 23:43:10 +0200

    GradesAveragesSection displaying averages & grades

--- client/src/components/GradesAveragesSection/GradesAveragesSection.js
+++ client/src/components/GradesAveragesSection/GradesAveragesSection.js
@@ -1,6 +1,59 @@
-import "./GradesAveragesSection"
+import "./GradesAveragesSection.css"
 
 const GradesAveragesSection = ({ gradesData }) => {
+
+    if(!gradesData.length > 0) return "";
+
+    const gradesTable = {
+        "0": 0,
+        "1": 1,
+        "1+": 1.5,
+        "2-": 1.75,
+        "2": 2,
+        "2+": 2.5,
+        "3-": 2.75,
+        "3": 3,
+        "3+": 3.5,
+        "4-": 3.75,
+        "4": 4,
+        "4+": 4.5,
+        "5-": 4.75,
+        "5": 5,
+        "5+": 5.5,
+        "6-": 5.75,
+        "6": 6
+    }
+
+    function calculateAverage(grades) {
+        let filteredGrades = grades.filter((grade) => grade.GradeCountToTheAverage === true);
+        filteredGrades = filteredGrades.filter((grade) => gradesTable[grade.Grade] != null);
+
+        if (filteredGrades.length === 0) return "-";
+
+        const sumOfWeights = filteredGrades.reduce((sum, grade) => sum + grade.GradeWeight, 0);
+        const sumOfGradesWithWeights = filteredGrades.reduce((sum, grade) => sum + (gradesTable[grade.Grade] * grade.GradeWeight), 0);
+
+        const average = sumOfGradesWithWeights / sumOfWeights;
+
+        return average.toFixed(2);
+    }
+
+    function getFinalGrade(grades) {
+        const finalGrade = grades.find((grade) => grade.IsFinal === true);
+
+        if (!finalGrade) {
+            return "-";
+        }
+
+        return finalGrade.Grade;
+    }
+
+    const grades = Object.values(gradesData);
+
+    const proposedAverage = calculateAverage(grades);
+    const finalAverage = 0;
+    const proposedGrade = 0;
+    const finalGrade = getFinalGrade(grades);
 
     return (<div className="grades-averages">
 
@@ -8,13 +61,11 @@ const GradesAveragesSection = ({ gradesData }) => {
             <tbody>
                 <tr>
                     <th>Semestralna</th>
-                    <td>Średnia: </td>
-                    <td>Wystawiona: </td>
                 </tr>
                 <tr>
-                    <th>Roczna</th>
-                    <td>Średnia: </td>
-                    <td>Wystawiona: </td>
+                    <td>Średnia: {proposedAverage}</td>
+                    <td>Proponowana: {proposedAverage}</td>
+                    <td>Wystawiona: {finalGrade} </td>
                 </tr>
             </tbody>
         </table>
