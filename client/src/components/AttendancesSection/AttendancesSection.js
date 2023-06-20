4ab1c7d1fb0b12acd05940e97f5cda351e31389e
xsiadron <xsiadronmc@gmail.com>
2023-06-20 13:01:23 +0200

    attendances count

--- client/src/components/AttendancesSection/AttendancesSection.js
+++ client/src/components/AttendancesSection/AttendancesSection.js
@@ -2,10 +2,23 @@ import "./AttendancesSection.css"
 import ProgressBar from "../ProgressBar/ProgressBar"
 
 const AttendancesSection = ({ attendancesData }) => {
-    const attendancePercentage = (attendancesData.Summary[2]["Obecność"] / attendancesData.Summary[2]["Quantity"]) * 100;
+    const attendances = attendancesData.Summary[2];
+    const attendancePercentage = (attendances["Obecność"] / attendances["Quantity"]) * 100;
+    let attendanceQuantity = attendances["Quantity"];
+
+    let attendanceQuantityString = "";
+    for (let attendance in attendances) {
+        if (attendance != "Quantity") {
+            if (attendance.toLowerCase() == "obecność") attendanceQuantityString += attendances[attendance] + "ob  "
+            else if (attendance.toLowerCase() == "nieobecność") attendanceQuantityString += attendances[attendance] + "nb  "
+            else if (attendance.toLowerCase() == "nieobecność usprawiedliwiona") attendanceQuantityString += attendances[attendance] + "nu  "
+            else if (attendance.toLowerCase() == "spóźnienie") attendanceQuantityString += attendances[attendance] + "sp  "
+        }
+    }
 
     return (<div className="attendances-section">
         <ProgressBar color={"#51FF35"} percentage={attendancePercentage} />
+        <p className="attendancesText">{attendanceQuantityString}</p>
     </div>)
 }
 
