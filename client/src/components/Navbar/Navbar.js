96812ff62feddca3f58031fd81d60412ac8d31d8
xsiadron <xsiadronmc@gmail.com>
2024-03-08 19:34:13 +0100

    Logo & navbar size

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import './Navbar.css';
 import logoutIcon from '../../icons/logout.svg';
+import librusStatisticsIcon from '../../icons/LibrusStatisticsIcon.png';
 import { useContext } from "react"
 import { AuthContext } from '../../AuthContext';
 
@@ -30,7 +31,15 @@ const Navbar = () => {
         <>
             <div className='nav-placeholder'></div>
             <nav className="nav">
-                <a href="/" className="site-title">Librus Statistics</a>
+                <div className="title-links-section">
+                    <a href="https://xsiadron.com" className="site-title">
+                        <img src={librusStatisticsIcon} height={60} />
+                        <div>
+                            <b>xsiadron.com</b><br />
+                            <small>Librus Statistics</small>
+                        </div>
+                    </a>
+                </div>
                 {isLogged && (
                     <a className="site-logout" onClick={logout}></a>
                 )}
